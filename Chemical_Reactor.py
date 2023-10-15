c694aa00afd46cdf96926f456cb0a39c81052ff6
Rupa Dey <68509719+Rupa-Dey@users.noreply.github.com>
2023-10-15 09:35:48 +0600

    Update Chemical_Reactor.py

--- Chemical_Reactor.py
+++ Chemical_Reactor.py
@@ -10,8 +10,8 @@
 #_____________________________________________________
 
 import matplotlib.pyplot as plt
-time = 100
-N = 500
+
+N = 100
 t = 0.2
 a = 1.0
 b = 0.5
@@ -26,19 +26,25 @@ print("Time\t\t\tA(i)\t\t\tB(i)\t\t\tC(i)")
 print(0.00,a,b,c,sep="\t\t\t")
 
 dt = t
+legend = False
 while dt<=N:
     a = a + k*(c-a*b)*dt
     b = b + k*(c-a*b)*dt
     c = c + 2*k*(a*b-c)*dt
     print(f"{dt:.2f}",f"{a:.2f}",f"{b:.2f}",f"{c:.2f}",sep="\t\t\t")
+
+
+
     A.append(a)
     B.append(b)
     C.append(c)
     dt += t
-
-plt.plot(A,label = "a")
-plt.plot(B,label = "b")
-plt.plot(C,label = "c")
-
-plt.legend(title = "title")
+    print(f"{dt:.2f}", f"{a:.2f}", f"{b:.2f}", f"{c:.2f}", sep="\t\t\t")
+    plt.plot(A,label = "a")
+    plt.plot(B,label = "b")
+    plt.plot(C,label = "c")
+    plt.pause(0.1)
+    if legend==False:
+        legend = True
+        plt.legend(title = "title")
 plt.show()
